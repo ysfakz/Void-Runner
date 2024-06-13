625e5df090450dfcf7b1257250b7ef2aab3bab8d
ysfakz <yusuf12pegasus@live.com>
2024-06-13 16:28:42 +0200

    limit amount of prefabs spawned

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -6,7 +6,9 @@ public class GroundSpawner : MonoBehaviour {
     public static GroundSpawner Instance { get; private set; }
     [SerializeField] private List<GameObject> groundPrefabs;
     [SerializeField] private float spawnInterval = 1f;
+    [SerializeField] private int maxprefabs = 11;
     private Transform lastSpawnedTransform;
+    private int spawnedAmount = 0;
 
     private void Awake() {
         Instance = this;
@@ -17,7 +19,7 @@ public class GroundSpawner : MonoBehaviour {
     }
 
     private IEnumerator SpawnGround() {
-        while (true) {
+        while (spawnedAmount < maxprefabs) {
             yield return new WaitForSeconds(spawnInterval);
 
             int index = Random.Range(0, groundPrefabs.Count);
@@ -30,9 +32,10 @@ public class GroundSpawner : MonoBehaviour {
                     spawnPos = nextspawnPoint.position;
                 }
             }
-
+            
             GameObject spawnedPrefab = Instantiate(prefabToSpawn, spawnPos, Quaternion.identity);
             lastSpawnedTransform = spawnedPrefab.transform;
+            spawnedAmount++;
         }
     }
 }
