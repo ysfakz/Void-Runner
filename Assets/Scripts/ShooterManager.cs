36b1b5366fbd44fa58d4ddc7e7236fa3136e7f45
ysfakz <yusuf12pegasus@live.com>
2024-06-19 15:32:59 +0200

    rockets being spawned

---
+++ Assets/Scripts/ShooterManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterManager : MonoBehaviour {
+
+    [SerializeField] List<GameObject> rocketPrefabs;
+    [SerializeField] List<Transform> shooters;
+    [SerializeField] private float minSpawnInterval = 2f;
+    [SerializeField] private float maxSpawnInterval = 5f;
+    [SerializeField] private float shootForce = 2f;
+    private float spawnTimer;
+
+    private void Start() {
+        ResetSpawnTimer();
+    }
+
+    private void Update() {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f) {
+            ShootRocket();
+            ResetSpawnTimer();
+        }
+    }
+
+    private void ResetSpawnTimer() {
+        spawnTimer = Random.Range(minSpawnInterval, maxSpawnInterval);
+    }
+
+    private void ShootRocket() {
+        if (rocketPrefabs.Count == 0 || shooters.Count == 0) {
+            return;
+        }
+
+        GameObject currentRocketPrefab = rocketPrefabs[Random.Range(0, rocketPrefabs.Count)];
+        Transform currentShooter = shooters[Random.Range(0, shooters.Count)];
+
+        Instantiate(currentRocketPrefab, currentShooter.position, currentShooter.rotation, transform);
+        
+    }
+
+}
