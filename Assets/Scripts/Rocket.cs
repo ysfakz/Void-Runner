4862463e425dede40bcc388d141c546ef82d784e
ysfakz <yusuf12pegasus@live.com>
2024-06-20 13:05:13 +0200

    rocket selfdestruct

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float lifetime = 10f;
+
+    private void Start() {
+        Invoke("DestroySelf", lifetime);
+    }
 
     private void Update() {
         transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
+    }
+
+    private void DestroySelf() {
+        Destroy(gameObject);
     }
 
 }
