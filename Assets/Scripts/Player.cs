e95190a5f69c7bb705e7147102d520285dd24006
ysfakz <yusuf12pegasus@live.com>
2024-06-25 12:29:31 +0200

    player animation according to game state (needs tweaking)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (GameManager.Instance.IsWaitingToStart()) {
+            animator.SetBool("IsWalking", false);
+        }
+        if (GameManager.Instance.IsGamePlaying()) {
+            animator.SetBool("IsWalking", true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other) {
         Rocket rocket = other.gameObject.GetComponent<Rocket>();
         if (rocket != null) {
