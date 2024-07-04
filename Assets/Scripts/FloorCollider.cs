daf99b04265e4c499c2575420a820a08b04fc18c
ysfakz <yusuf12pegasus@live.com>
2024-07-04 10:52:09 +0200

    ya 45 remove unused imports

--- Assets/Scripts/FloorCollider.cs
+++ Assets/Scripts/FloorCollider.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FloorCollider : MonoBehaviour {
@@ -13,14 +11,4 @@ public class FloorCollider : MonoBehaviour {
             }
         }
     }
-
-    // private void OnTriggerExit(Collider other) {
-    //     Player player = other.GetComponent<Player>();
-    //     if (player != null) {
-    //         GroundMove parent = GetComponentInParent<GroundMove>();
-    //         if (parent != null) {
-    //             parent.PlayerExit();
-    //         }
-    //     }
-    // }
 }
